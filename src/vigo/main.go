913021ccdc21196bcf9493542479351add03e3b5
Daan <daandenhoed@gmail.com>
2024-07-06 11:33:28 +0200

    renew

--- src/vigo/main.go
+++ src/vigo/main.go
@@ -1,38 +1,10 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "os"
 )
 
-func enableRawMode() {
-  // get terminal state
-
-  // make adjustments
-
-  // set terminal state
-
-}
 
 func main() {
-  enableRawMode()
 
-  reader := bufio.NewReader(os.Stdin)
-
-  for {
-    c, err := reader.ReadByte()
-    if err != nil {
-      // if error including EOF, break
-      break
-    }
-    if c == 'q' {
-      // if 'q' is entered, break
-      break
-    }
-
-    fmt.Printf("%c", c)
-  }
 
 }
-
